1a9dcbe2db4f82147e296e8126fcfcfb386fe2ca
Jelena Mikovic <jelena.mikovic189@gmail.com>
2023-03-26 18:25:52 +0200

    Initialized project

---
+++ Tim17_Back/src/main/java/com/ib/Tim17_Back/Tim17BackApplication.java
@@ -0,0 +1,13 @@
+package com.ib.Tim17_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tim17BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tim17BackApplication.class, args);
+	}
+
+}
---
+++ Tim17_Back/src/test/java/com/ib/Tim17_Back/Tim17BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ib.Tim17_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tim17BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
