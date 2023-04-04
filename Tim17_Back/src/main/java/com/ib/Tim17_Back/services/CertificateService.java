387d5c398d569076043e2cfe45d8c15ee2fc6a01
Jelena Mikovic <jelena.mikovic189@gmail.com>
2023-04-04 04:59:10 +0200

    Added validation for certificate

--- Tim17_Back/src/main/java/com/ib/Tim17_Back/services/CertificateService.java
+++ Tim17_Back/src/main/java/com/ib/Tim17_Back/services/CertificateService.java
@@ -1,14 +1,18 @@
 package com.ib.Tim17_Back.services;
 
 import com.ib.Tim17_Back.dtos.CertificateDTO;
+import com.ib.Tim17_Back.exceptions.CustomException;
 import com.ib.Tim17_Back.models.Certificate;
 import com.ib.Tim17_Back.repositories.CertificateRepository;
 import com.ib.Tim17_Back.services.interfaces.ICertificateService;
 import org.hibernate.Hibernate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CertificateService implements ICertificateService {
@@ -28,5 +32,27 @@ public class CertificateService implements ICertificateService {
             certificates.add(new CertificateDTO(certificate));
         }
         return certificates;
+    }
+
+    @Override
+    public boolean isValid(Long id) {
+        Certificate certificate = null;
+        Optional<Certificate> optionalCertificate = certificateRepository.findById(id);
+        if(optionalCertificate.isPresent()) certificate = optionalCertificate.get();
+        else throw new CustomException("Certificate not found");
+        if(!certificate.isValid()) return false;
+        if(certificate.getEndDate().isBefore(LocalDateTime.now()))
+        {
+            certificate.setValid(false);
+            certificateRepository.save(certificate);
+            return false;
+        }
+        if(certificate.getIssuer() != null)
+            if(!this.isValid(certificate.getIssuer().getId())) {
+                certificate.setValid(false);
+                certificateRepository.save(certificate);
+                return false;
+            }
+        return true;
     }
 }
