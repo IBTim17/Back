7bb352e297e8e7178bacaf6d87c8547283a90fe7
Jana Nikolic <jananikolic25@gmail.com>
2023-04-05 04:15:14 +0200

    Updated import.sql

--- Tim17_Back/src/main/resources/import.sql
+++ Tim17_Back/src/main/resources/import.sql
@@ -1,7 +1,4 @@
--- INSERT INTO ROLE (name) VALUES ('USER');
--- INSERT INTO ROLE (name) VALUES ('ADMIN');
-INSERT INTO users(first_name, last_name, phone_number, email, role, password, password_last_changed, is_activated) VALUES ('Zika', 'Peric', '+381698566565', 'zika@peric.com', 'ADMIN', '$2a$12$G/IJUGIXL9ohQeMMpXwuM.wpRKKDYKOWqS9LK/ra2jlq0.QSt4j6a', '2023-04-04 22:58:40.985741', true);
-INSERT INTO users(first_name, last_name, phone_number, email, role, password, password_last_changed, is_activated) VALUES ('Mika', 'Peric', '+381618566565', 'mika@peric.com', 'ADMIN', '$2a$12$G/IJUGIXL9ohQeMMpXwuM.wpRKKDYKOWqS9LK/ra2jlq0.QSt4j6a', '2023-04-04 22:58:40.985741', true);
 
--- insert into users (email, first_name, is_activated, last_name, password, password_last_changed, phone_number, role)
--- values ('Zika', 'Peric', '+381698566565', 'zika@peric.com', 'ADMIN', '$2a$12$G/IJUGIXL9ohQeMMpXwuM.wpRKKDYKOWqS9LK/ra2jlq0.QSt4j6a', '2023-04-04 22:58:40.985741', true);
+INSERT INTO users(first_name, last_name, phone_number, email, role, password, password_last_changed, is_activated) VALUES ('Zika', 'Peric', '+381698566565', 'zika@peric.com', 'ADMIN', '$2a$10$96kYkpV7uY1IhFsElBfFcuwM2uxQr43ArA/ZQRdmQApipcT6LzV.a', '2023-04-04 22:58:40.985741', true);
+INSERT INTO users(first_name, last_name, phone_number, email, role, password, password_last_changed, is_activated) VALUES ('Mika', 'Peric', '+381618566565', 'mika@peric.com', 'ADMIN', '$2a$10$96kYkpV7uY1IhFsElBfFcuwM2uxQr43ArA/ZQRdmQApipcT6LzV.a', '2023-04-04 22:58:40.985741', true);
+INSERT INTO users(first_name, last_name, phone_number, email, role, password, password_last_changed, is_activated) VALUES ('Luka', 'Perovic', '+381665898565', 'luka@perovic.com', 'USER', '$2a$10$96kYkpV7uY1IhFsElBfFcuwM2uxQr43ArA/ZQRdmQApipcT6LzV.a', '2023-04-04 22:58:40.985741', true);
