9d4698f0b4a07600f0b66a4006dd91000f65ebb6
Jana Nikolic <jananikolic25@gmail.com>
2023-05-07 21:39:12 +0200

    Added endpoint for downloading certificates

--- Tim17_Back/src/main/java/com/ib/Tim17_Back/controllers/CertificateController.java
+++ Tim17_Back/src/main/java/com/ib/Tim17_Back/controllers/CertificateController.java
@@ -1,13 +1,21 @@
 package com.ib.Tim17_Back.controllers;
 
 import com.ib.Tim17_Back.dtos.CertificateDTO;
+import com.ib.Tim17_Back.models.Certificate;
 import com.ib.Tim17_Back.services.interfaces.ICertificateService;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 @Controller
@@ -35,4 +43,25 @@ public class CertificateController {
         return new ResponseEntity<>(this.certificateService.isValid(serialNumber), HttpStatus.OK);
     }
 
+    @GetMapping(value = "/download/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    public ResponseEntity<InputStreamResource> download(@PathVariable String id) {
+        File file = certificateService.getFileBySerialNumber(id);
+        InputStream inputStream = null;
+
+        try {
+            inputStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=" + file.getName());
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(new InputStreamResource(inputStream));
+    }
 }
