1b10da2599a6746e164c7ef52a9623944e844ad8
Jana Nikolic <jananikolic25@gmail.com>
2023-06-17 15:04:16 +0200

    Bug fix

--- Tim17_Back/src/main/resources/import.sql
+++ Tim17_Back/src/main/resources/import.sql
@@ -1,7 +1,7 @@
 INSERT INTO users(first_name, last_name, phone_number, email, role, password, is_activated) VALUES ('Zika', 'Peric', '+381698566565', 'jananikolic25+5@gmail.com', 'ADMIN', '$2a$10$W7975e8lrqsDDewmmTwebevnDBMDs3JgA9Xg9Sb8.rSGa1rlZK4P.', true);
-INSERT INTO password_user(user_id, password, date) VALUES (1, '$2a$10$W7975e8lrqsDDewmmTwebevnDBMDs3JgA9Xg9Sb8.rSGa1rlZK4P.', '2023-01-25');
+INSERT INTO password_user(user_id, password, date) VALUES (1, '$2a$10$W7975e8lrqsDDewmmTwebevnDBMDs3JgA9Xg9Sb8.rSGa1rlZK4P.', '2023-06-15');
 INSERT INTO users(first_name, last_name, phone_number, email, role, password, is_activated) VALUES ('Mika', 'Peric', '+381618566565', 'mika@peric.com', 'ADMIN', '$2a$10$96kYkpV7uY1IhFsElBfFcuwM2uxQr43ArA/ZQRdmQApipcT6LzV.a', true);
-INSERT INTO users(first_name, last_name, phone_number, email, role, password, is_activated) VALUES ('Luka', 'Perovic', '+381665898565', 'luka@perovic.com', 'USER', '$2a$10$96kYkpV7uY1IhFsElBfFcuwM2uxQr43ArA/ZQRdmQApipcT6LzV.a', true);
+INSERT INTO users(first_name, last_name, phone_number, email, role, password, is_activated) VALUES ('Luka', 'Perovic', '+381665898565', 'jananikolic25+1@gmail.com', 'USER', '$2a$10$96kYkpV7uY1IhFsElBfFcuwM2uxQr43ArA/ZQRdmQApipcT6LzV.a', true);
 INSERT INTO users(first_name, last_name, phone_number, email, role, password , is_activated) VALUES ('Jana', 'Nikolic', '+381654277561', 'jananikolic25@gmail.com', 'ADMIN', '$2a$10$96kYkpV7uY1IhFsElBfFcuwM2uxQr43ArA/ZQRdmQApipcT6LzV.a', true);
 INSERT INTO password_user(user_id, password, date) VALUES (2, '$2a$10$W7975e8lrqsDDewmmTwebevnDBMDs3JgA9Xg9Sb8.rSGa1rlZK4P.', '2023-01-25');
 INSERT INTO password_user(user_id, password, date) VALUES (3, '$2a$10$W7975e8lrqsDDewmmTwebevnDBMDs3JgA9Xg9Sb8.rSGa1rlZK4P.', '2023-01-25');
